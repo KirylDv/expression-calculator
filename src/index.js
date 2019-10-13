5e84bf6e56d63453195c0674e4c119a7af7c05d4
KirylDv <39598590+KirylDv@users.noreply.github.com>
2019-10-13 21:01:11 +0300

    add: errors, RPN function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,48 @@ function eval() {
     return;
 }
 
+function rpn(expr) {
+    expr = sya(expr);
+    const operators = {
+        '+': (x, y) => x + y,
+        '-': (x, y) => x - y,
+        '*': (x, y) => x * y,
+        '/': (x, y) => x / y
+    };
+
+    let stack = [];
+    expr.split(' ').forEach((token) => {
+        if (token in operators) {
+            let y = stack.pop();
+            let x = stack.pop();
+            if(token === "/" && y === 0) {
+                throw("TypeError: Division by zero.");
+            }
+            stack.push(operators[token](x, y));
+        } else {
+            stack.push(+token);
+        }
+    });
+
+    return stack.pop();
+}
+
 function expressionCalculator(expr) {
-    // write your solution here
+    let a = 0;
+    for (let char of expr) {
+        if (char === "("){
+            a++;
+        }
+        else if (char === ")") {
+            a--;
+        }
+    }
+    if(a !== 0) {
+        throw("ExpressionError: Brackets must be paired");
+    }
+
+
+    return rpn(expr);
 }
 
 module.exports = {
